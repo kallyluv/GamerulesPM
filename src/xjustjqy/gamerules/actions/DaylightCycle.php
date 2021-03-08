1c00cf127ef73d64db5024cffcc585db9fecffeb
xJustJqy <78117567+xJustJqy@users.noreply.github.com>
2021-03-08 10:36:37 -0800

    Create DaylightCycle.php

---
+++ src/xjustjqy/gamerules/actions/DaylightCycle.php
@@ -0,0 +1,43 @@
+<?
+
+namespace xjustjqy\gamerules\actions;
+
+use xjustjqy\gamerules\Main;
+
+use pocketmine\plugin\Plugin;
+use pocketmine\Server;
+
+use pocketmine\scheduler\ClosureTask;
+
+use Closure;
+
+function setInterval(Closure $callback, int $delay) {
+  Main::fetch()->getScheduler()->scheduleRepeatingTask($task = new ClosureTask($callback), $delay);
+  return $task;
+}
+
+class DaylightCycle extends Action {
+  
+    /** @var Plugin */
+    private $plugin;
+
+    private $lastTime;
+ 
+    public function __construct(Plugin $register) {
+      $this->register($register); 
+      $this->plugin = $register;
+      setInterval(function() {
+        if(!$this->isCancelled()) return;
+        $level = Server::getInstance()->getDefaultLevel();
+        $time = $level->getTime();
+        if(!isset($this->lastTime)) $this->lastTime = $time;
+        foreach(Server::getInstance()->getLevels() as $level) {
+          $time = $level->getTime();
+          if($this->lastTime !== $time) {
+            $level->setTime($time);
+          }
+        }
+      }, 20);
+    }
+  
+}
