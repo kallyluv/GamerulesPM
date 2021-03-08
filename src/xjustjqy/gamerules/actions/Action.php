74f854c77d22575e8b32415f5cf1ff629977efdb
xJustJqy <78117567+xJustJqy@users.noreply.github.com>
2021-03-08 09:48:21 -0800

    Added dynamic listeners

--- src/xjustjqy/gamerules/actions/Action.php
+++ src/xjustjqy/gamerules/actions/Action.php
@@ -6,13 +6,30 @@ use pocketmine\event\Listener;
 use pocketmine\plugin\Plugin;
 use pocketmine\Server;
 
+use pocketmine\event\Event;
+
 class Action implements Listener {
  
     /** @var bool */
     private $cancel = false;
+    /** @var array */
+    private $listners = [];
  
     public function register(Plugin $register) {
        Server::getInstance()->getPluginManager()->registerEvents($this, $register);
+    }
+ 
+    public function addListener($listener, $callback) {
+      if(!isset($this->listeners[$listener])) $this->listeners[$listener] = [];
+      $this->listeners[$listener][] = $callback;
+    }
+ 
+    public function onEvent(Event $event) {
+      $name = $event->getEventName();
+      if(is_null($name)) return;
+      foreach($this->listeners[$name] as $callback) {
+        $this->{$callback}($event);
+      }
     }
   
     public function isCancelled() {
