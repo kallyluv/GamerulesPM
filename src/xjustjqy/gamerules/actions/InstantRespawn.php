4e6c53bb93563d81ccf560700818776e9494380c
xJustJqy <78117567+xJustJqy@users.noreply.github.com>
2021-03-10 09:19:48 -0800

    Fixed InstantRespawn

--- src/xjustjqy/gamerules/actions/InstantRespawn.php
+++ src/xjustjqy/gamerules/actions/InstantRespawn.php
@@ -1,31 +1,44 @@
 <?
 
 namespace xjustjqy\gamerules\actions;
+use xjustjqy\gamerules\Main;
 
 use pocketmine\plugin\Plugin;
 use pocketmine\Server;
+use pocketmine\Player;
 
 use pocketmine\event\player\PlayerDeathEvent;
-
-use pocketmine\network\mcpe\protocol\RespawnPacket;
+use pocketmine\event\entity\EntityDamageEvent;
 
 class InstantRespawn extends Action {
   
   public function __construct(Plugin $register) {
     $this->register($register);
-    $this->addListener("PlayerDeathEvent", "onDeath");
+    $this->addListener("EntityDamageEvent", "onDeath");
   }
   
-  public function onDeath(PlayerDeathEvent $event) {
-    if($this->isCancelled()) {
-      $player = $event->getPlayer();
-      $pk = new RespawnPacket();
-      $pk->entityRuntimeId = $player->getId();
-      $pk->position = Server::getInstance()->getDefaultLevel()->getSafeSpawn();
-      $pk->readyState = 1;
-      foreach(Server::getInstance()->getOnlinePlayers() as $p) {
-        $p->dataPacket($pk); 
+  public function onDeath(EntityDamageEvent $event) {
+    $player = $event->getEntity();
+    if($this->isCancelled() && $player instanceof Player && !$event->isCancelled() && $event->getFinalDamage() >= ($player->getHealth() - 0.1)) {
+      $event->setCancelled(true);
+      $plugin = Main::fetch();
+      $armor = null;
+      $inv = null;
+      if(!isset($plugin->gamerules["keepinventory"]) || !$plugin->gamerules["keepinventory"]) {
+        $armor = $player->getArmorInventory()->getContents();
+        $inv = $player->getInventory()->getContents();
       }
+      $ev = new PlayerDeathEvent($player, []);
+      $ev->call();
+      $player->setHealth($player->getMaxHealth());
+      $player->setFood($player->getMaxFood());
+      if($armor !== null && is_array($armor)) {
+        $player->getArmorInventory()->setContents($armor);
+      }
+      if($inv !== null && is_array($inv)) {
+        $player->getInventory()->setContents($inv); 
+      }
+      $player->teleport($player->getSpawn());
     }
   }
   
